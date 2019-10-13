dd340f34a09e7c81cb6a5bb4f3bd21af2ee9eba3
nlmhc <56232310+nlmhc@users.noreply.github.com>
2019-10-13 13:53:30 +0800

    Create statistics.js

---
+++ statistics.js
@@ -0,0 +1,2 @@
+var code = '<script type="text/javascript" src="https://v1.cnzz.com/z_stat.php?id=1278104554&web_id=1278104554"></script>';
+document.write('<div style="display:none;">' + code + '</div>');
